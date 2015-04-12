f35533d19f25bb20a7dfc2eab32f16e5ecbb47a6
Dmitrii Meleshko <Dmitrii@MeleshkoDima.(none)>
2015-04-12 22:05:50 +0300

    Assembly paremeters added

--- release_script.sh
+++ release_script.sh
@@ -12,3 +12,37 @@ correct -f ./data/readnames.txt -k 17 -c 6 -m readnames.txt.qcts -p 4
 #Reads are stored in ./data/R1.cor.fq and ./data/R2.cor.fq 
 rm kmers.txt error_model* r.log readnames.txt.qcts
 
+#Now we start assembly with SGA
+#----------------ASSEMBLY STEP----------------------------
+
+#
+# Example assembly of 100bp E.coli data set. 
+#
+IN1=./data/R1.cor.fq
+IN2=./data/R2.cor.fq
+
+# Overlap parameter used for the final assembly. This is the only argument
+# to the script
+OL=55
+# The number of threads to use
+CPU=8
+# To save memory, we index $D reads at a time then merge the indices together
+D=4000000
+# Correction k-mer value
+CK=33
+# The minimum k-mer coverage for the filter step. Each 27-mer
+# in the reads must be seen at least this many times
+COV_FILTER=2
+# Overlap parameter used for FM-merge. This value must be no greater than the minimum
+# overlap value you wish to try for the assembly step.
+MOL=45
+# Parameter for the small repeat resolution algorithm
+R=10
+# The number of pairs required to link two contigs into a scaffold
+MIN_PAIRS=1
+# The minimum length of contigs to include in a scaffold
+MIN_LENGTH=100
+# Distance estimate tolerance when resolving scaffold sequences
+SCAFFOLD_TOLERANCE=1
+# Turn off collapsing bubbles around indels
+MAX_GAP_DIFF=0.05
