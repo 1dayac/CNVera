ff991fcd0189c23f31a4fc0d586ab68a114adbda
Dmitrii Meleshko <Dmitrii@MeleshkoDima.(none)>
2015-04-12 21:21:56 +0300

    Read correction instruction completed

--- release_script.sh
+++ release_script.sh
@@ -1,10 +1,11 @@
 #First you need to do is simulate reads from reference
 #For this purpose we used art - http://www.niehs.nih.gov/research/resources/software/biostatistics/art/  
 #Please export art_illumina to paths
-cd data
-art_illumina -i DH10B-K12.fasta -p -f 25 -l 100 -m 300 -s 50 -qs 50 -qs2 50 -o R
+art_illumina -i ./data/DH10B-K12.fasta -p -f 25 -l 100 -m 300 -s 50 -qs 50 -qs2 50 -o ./data/R
 #Then for read correction start Quake
-echo -e "R1.fq\nR2.fq" >readnames.txt
-python quake.py -f readnames.txt -k 17
-
-cd ..
+echo -e "./data/R1.fq\n./data/R2.fq" >./data/readnames.txt
+count_qmers -f ./data/readnames.txt -k 17 -q 64
+#as we know coverage lets chose value like 6, because quake often fails to di it by itself
+correct -f ./data/readnames.txt -k 17 -c 6 -m readnames.txt.qcts -p 4
+#Reads are stored in ./data/R1.cor.fq and ./data/R2.cor.fq 
+rm kmers.txt error_model* r.log readnames.txt.qcts
