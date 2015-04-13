ea3f2a43f60a9714dc2be7cf4645b1c46f24a718
1dayac <1dayac@rambler.ru>
2015-04-13 12:55:41 +0300

    Summary added

--- tools/solutionComparer.py
+++ tools/solutionComparer.py
@@ -36,7 +36,7 @@ lengthes_long = []
 
 answer = defaultdict(int)
 
-
+f3 = open("summary.txt", 'w')
 for row in blast_file.readlines():
     splitted_row = row.split()
     if splitted_row[0] not in lengthes and splitted_row[0] not in lengthes_just and float(splitted_row[2]) > 95.0 and splitted_row[3] == splitted_row[7] and splitted_row[6] == "1" and splitted_row[4] == "0" and splitted_row[5] == "0" :
@@ -50,7 +50,7 @@ for row in blast_file.readlines():
     if splitted_row[0] in lengthes and float(lengthes[splitted_row[0]]) ==  float(splitted_row[3]) and float(splitted_row[2]) > 95.0:
         answer[splitted_row[0]] += 1
     lengthes_just[splitted_row[0]] = 1
-f1 = open("difference_first.txt", 'w')
+f1 = open("difference_magnolia.txt", 'w')
 
 difference_first = 0
 difference_first_short = 0
@@ -83,19 +83,21 @@ for i in answer.keys():
             difference_first += abs(answer[i] - first_solution[i])
             f1.write(i + " " + str(answer[i] - first_solution[i]) + "\n")
 
-f1.write("Total difference " + str(difference_first) + "\n")
-f1.write("Total difference short <250bp " + str(difference_first_short) + "\n")
-f1.write("True short <250bp " + str(true_first_short) + "\n")
+f3.write("Summary for Magnolia")
+f3.write("Total difference " + str(difference_first) + "\n")
+f3.write("Total difference short <250bp " + str(difference_first_short) + "\n")
+f3.write("True short <250bp " + str(true_first_short) + "\n")
 
 
-f1.write("Total difference medium 250-1000bp " + str(difference_first_medium) + "\n")
-f1.write("True medium 250-1000bp " + str(true_first_medium) + "\n")
+f3.write("Total difference medium 250-1000bp " + str(difference_first_medium) + "\n")
+f3.write("True medium 250-1000bp " + str(true_first_medium) + "\n")
 
-f1.write("Total difference long >1000 bp " + str(difference_first_long) + "\n")
-f1.write("True long >1000bp " + str(true_first_long) + "\n")
+f3.write("Total difference long >1000 bp " + str(difference_first_long) + "\n")
+f3.write("True long >1000bp " + str(true_first_long) + "\n")
 
 
-f2 = open("difference_second.txt", 'w')
+f2 = open("difference_CNVera.txt", 'w')
+f3.write("=============================")
 
 difference_second = 0
 difference_second_short = 0
@@ -128,6 +130,7 @@ for i in answer.keys():
             difference_second += abs(answer[i] - second_solution[i])
             f2.write(i + " " + str(answer[i] - second_solution[i]) + "\n")
 
+f3.write("Summary for CNVera")
 f2.write("Total difference " + str(difference_second) + "\n")
 f2.write("Total difference short <250bp " + str(difference_second_short) + "\n")
 f2.write("True short <250bp " + str(true_second_short) + "\n")
@@ -137,7 +140,9 @@ f2.write("True medium 250-1000bp " + str(true_second_medium) + "\n")
 
 f2.write("Total difference long >1000 bp " + str(difference_second_long) + "\n")
 f2.write("True long >1000bp " + str(true_second_long) + "\n")
+f3.write("==============================")
 
+f3.write("Total summary")
 f2.write("Total short - " + str(len(lengthes_short)) + "\n")
 f2.write("Total medium - " + str(len(lengthes_medium)) + "\n")
 f2.write("Total long -  " + str(len(lengthes_long)) + "\n")
